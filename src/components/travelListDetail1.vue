9bd20d37f720a98a42ef95f9d5bb1ce77115228a
ThanyaphornSaetao347 <142294145+ThanyaphornSaetao347@users.noreply.github.com>
2023-10-05 16:55:22 +0700

    Antttt

---
+++ src/components/travelListDetail1.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="col" v-for="(n) in travellist" :key="n">
+    <div class="name">
+      <p>{{ n.name }}</p>
+    </div>
+    <div class="tipark">
+      <RouterLink to="ที่พัก">ที่พัก</RouterLink> /
+      <RouterLink to="สถานที่เที่ยว">สถานที่เที่ยว</RouterLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router'
+const travellist=ref([
+{name: 'ดอยหลวงเชียงดาว',  price:500, img:"https://www.reviewchiangmai.com/wp-content/uploads/2022/06/doi-luang-chiang-dao-1536x1024.jpg", quantity: 0 ,
+text_box:'เดินทางโดยสวัสดิภาพ'
+}])
+
+
+</script>
+
+<style>
+.name{
+  text-align: center;
+}
+.tipark{
+  text-align: center;
+}
+
+</style>
---
+++ src/components/travelListDetail12.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="col" v-for="(n) in travellist" :key="n">
+    <div class="name">
+      <p>{{ n.name }}</p>
+    </div>
+    <div class="tipark">
+      <RouterLink to="ที่พัก">ที่พัก</RouterLink> /
+      <RouterLink to="สถานที่เที่ยว">สถานที่เที่ยว</RouterLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+import { ref } from 'vue';
+
+const travellist=ref([
+{name: 'ดอยม่อนเงาะ', img:"https://www.reviewchiangmai.com/wp-content/uploads/2022/06/doi-monngo.jpg", quantity: 0 ,
+text_box:'เดินทางโดยสวัสดิภาพ'
+}])
+
+
+</script>
+
+<style>
+
+.card-img-top{
+height: 100%;
+padding: 25px;
+
+}
+.box_all{
+justify-content: center;
+align-items: center;
+margin-left: 30%;
+text-align: center
+}
+
+</style>
